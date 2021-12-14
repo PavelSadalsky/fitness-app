a1eea5b48ac420117a49b2c05af68b9648d4db0b
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-12-14 05:39:15 +0300

    Chart fix.

--- core/Chart.py
+++ core/Chart.py
@@ -8,9 +8,10 @@ class Chart:
 
         # plot
         fig, ax = plt.subplots(figsize=(10, 8))
-        ax.set_xlabel('Количесвто месяцев')
+        ax.set_xlabel('Количество месяцев')
         ax.set_ylabel('Вес в килограммах')
 
         ax.plot(x, y, linewidth=8.0, color='orange', alpha=0.7)
 
         plt.savefig('chart.png', bbox_inches='tight')
+
