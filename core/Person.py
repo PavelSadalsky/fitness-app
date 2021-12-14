390971341d5042fbe77fc2a2185c480e930c3191
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-12-14 03:02:34 +0300

    New version.

--- core/Person.py
+++ core/Person.py
@@ -1,16 +1,27 @@
-from datetime import date
-
-
 class Person:
-    __date_of_birth: date
+    __age: int
+    __height: int
+    __weight: int
+    __desired_weight: int
+    __coefficients = {
+        1: 1.2,
+        2: 1.375,
+        3: 1.55,
+        4: 1.725,
+        5: 1.9
+    }
 
-    def __init__(self, first_name, last_name, sex):
-        self.__first_name: str = first_name
-        self.__last_name: str = last_name
+    def __init__(self, sex):
         self.__sex: bool = sex
 
-    def set_date_of_birth(self, year, month, day):
-        self.__date_of_birth = date(year, month, day)
+    def set_age(self, age):
+        self.__age = age
 
-    def get_age(self):
-        return int((date.today() - self.__date_of_birth).days / 365.2425)
+    def set_height(self, height):
+        self.__height = height
+
+    def set_weight(self, weight):
+        self.__weight = weight
+
+    def set_desired_weight(self, desired_weight):
+        self.__desired_weight = desired_weight
